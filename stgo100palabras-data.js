9293f643e83200b5528e6688c73a4506a816b22a
atelierdechile <atelierdechile@users.noreply.github.com>
2017-07-14 19:28:37 -0400

    Update stgo100palabras-data.js

--- stgo100palabras-data.js
+++ stgo100palabras-data.js
@@ -12,32 +12,20 @@ var treeData = [
                 "url":   "https://www.youtube.com/v/YEUwSUePz5k"} , 
                {"name":    "Mentes Brillantes 1 - Ramón Latorre",
                 "url":   "https://www.youtube.com/v/voogNkB14gM"}
- 
-]
- 
-},
-    
-{
+                          ]
+      },
+      {
             "name": "Ganadores 2016"
             ,"_children": [
-               {  
-                "name":   "Mentes Brillantes 2 - Jorge Allende",
-                 "url": "https://www.youtube.com/v/joXU0YgKu4Q"
-              },
-              {  
-                "name":   "Mentes Brillantes 2 - Patricio Felmer",
-                 "url": "https://www.youtube.com/v/NbxWbgtr0Ok"
-              },
-              {  
-                "name":   "Mentes Brillantes 2 - Andrés Weintraub",
-                 "url": "https://www.youtube.com/v/UoPAdtI8HRU"
-              },
-              {"name":    "Mentes Brillantes 2 - Ricardo Uauy"
-              ,"url":   "https://www.youtube.com/v/oNgIp0ifo0s"
-              }
-
- 
-       ]
+              {"name":   "Mentes Brillantes 2 - Jorge Allende",
+               "url": "https://www.youtube.com/v/joXU0YgKu4Q" },
+              {"name":   "Mentes Brillantes 2 - Patricio Felmer",
+               "url": "https://www.youtube.com/v/NbxWbgtr0Ok" },
+              {"name":   "Mentes Brillantes 2 - Andrés Weintraub",
+               "url": "https://www.youtube.com/v/UoPAdtI8HRU" },
+              {"name":   "Mentes Brillantes 2 - Ricardo Uauy",
+               "url":   "https://www.youtube.com/v/oNgIp0ifo0s" }
+                          ]
       } 
     ]
   }
