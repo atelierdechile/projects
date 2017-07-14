1ed5c3aab97af95dcec015be1e7177b27b70841b
atelierdechile <atelierdechile@users.noreply.github.com>
2017-07-14 19:03:37 -0400

    Update stgo100palabras-data.js

--- stgo100palabras-data.js
+++ stgo100palabras-data.js
@@ -4,52 +4,18 @@ var treeData = [
     "parent": "null",
     "children": [
       {
-            "name": "Temporada 1"
+            "name": "Ganadores 2017"
             ,"_children": [
                {  
-                "name":   "Mentes Brillantes 1 - Pablo Valenzuela",
-                 "url": "https://www.youtube.com/v/ZmzUksziaCg"
-              }
-             ,{  
-                "name":   "Mentes Brillantes 1 - Ricardo Baeza",
-                 "url": "https://www.youtube.com/v/36BXjU7qvPQ"
+                "name":   "Los Vampiros - Macarena Araya Lira, 31 años, Ñuñoa",
+                 "url": "http://santiagoen100palabras.cl/ganadores/2017/#1"
               },
-              {  
-                "name":   "Mentes Brillantes 1 - Carlos Conca",
-                 "url": "https://www.youtube.com/v/nRPOUSD1GU8"
-              }
-              ,
-              {  
-                "name":   "Mentes Brillantes 1 - Eric Goles",
-                 "url": "https://www.youtube.com/v/Nn0lXUqjhek"
-              }
-               ,
-              {  
-                "name":   "Mentes Brillantes 1 - Nibaldo Inestroza",
-                 "url": "https://www.youtube.com/v/lqfKONeRNg4"
-              }
-               ,
-              {  
-                "name":   "Mentes Brillantes 1 - Juan Asenjo",
-                 "url": "https://www.youtube.com/v/bWYJC4BzU6Y"
-              },
-              
-             {
-                "name":    "Mentes Brillantes 1 - María Teresa Ruiz"
-                ,"url":   "https://www.youtube.com/v/LYzGmPmu0vg"
-             },
-
-             { "name":    "Mentes Brillantes 1 - Mario Luxoro"
-             ,"url":   "https://www.youtube.com/v/YEUwSUePz5k"
+             { "name":    "Mentes Brillantes 1 - Mario Luxoro",
+             "url":   "https://www.youtube.com/v/YEUwSUePz5k"
              } , 
-
-             { "name":    "Mentes Brillantes 1 - Fernando Mönckeberg"
-             ,"url":   "https://www.youtube.com/v/BNas0FvnIyk"
-             },
-          
-              {
-              "name":    "Mentes Brillantes 1 - Ramón Latorre"
-              ,"url":   "https://www.youtube.com/v/voogNkB14gM"
+            {
+              "name":    "Mentes Brillantes 1 - Ramón Latorre",
+              "url":   "https://www.youtube.com/v/voogNkB14gM"
               }
  
 ]
@@ -62,34 +28,10 @@ var treeData = [
                {  
                 "name":   "Mentes Brillantes 2 - Jorge Allende",
                  "url": "https://www.youtube.com/v/joXU0YgKu4Q"
-              }
-              ,{  
-                "name":   "Mentes Brillantes 2 - Cecilia Hidalgo",
-                 "url": "https://www.youtube.com/v/BENmwqYWRVM"
-              },
-              {  
-                "name":   "Mentes Brillantes 2 - Juan Carlos Castilla",
-                 "url": "https://www.youtube.com/v/gKeUZgfPqC4"
-              },
-              {  
-                "name":   "Mentes Brillantes 2 - Fernando Lund",
-                 "url": "https://www.youtube.com/v/5LjWiK58Qus"
-              },
-             {  
-                "name":   "Mentes Brillantes 2 - Mary Kalin",
-                 "url": "https://www.youtube.com/v/d8Rzc3g2_2I"
               },
               {  
                 "name":   "Mentes Brillantes 2 - Patricio Felmer",
                  "url": "https://www.youtube.com/v/NbxWbgtr0Ok"
-              },
-              {  
-                "name":   "Mentes Brillantes 2 - Enrique Tirapegui",
-                 "url": "https://www.youtube.com/v/nKxFf4MMWbQ"
-              },
-              {  
-                "name":   "Mentes Brillantes 2 - Bernabé Santelices",
-                 "url": "https://www.youtube.com/v/1BqLqGkOkZ8"
               },
               {  
                 "name":   "Mentes Brillantes 2 - Andrés Weintraub",
@@ -100,11 +42,8 @@ var treeData = [
               }
 
  
-]
- 
-}
-       
-
+       ]
+      } 
     ]
   }
 ];
